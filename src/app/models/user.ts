0ca85ec7ddd3cf866bf7c3da885a3ac80e025b93
Youssef-zekry <youssef.zekry@mokalmat.com>
2023-07-25 16:41:52 +0300

    Add user attributes.

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,16 +1,29 @@
 export class user {
+	public status_code: number = 0;
+	public message: string = '';
+	public firstName: string = '';
+	public lastName: string = '';
+	public birthdate: Date | null = null;
+	public mail: string = '';
+	public gender: string = '';
+	public mobile: string = '';
+	public address: string = '';
+	public balance: number = 0;
+	public userid: number = 0;
+	public token: string = '';
 	constructor(
-	  public status_code: number,
-	  public message: string,
-	  public firstName: string,
-	  public lastName: string,
-	  public birthdate: Date,
-	  public mail: string,
-	  public gender: string,
-	  public mobile: string,
-	  public address: string,
-	  public balance: number,
-	  public userid: number,
-	  public token: string
+	   status_code?: number,
+	   message?: string,
+	   firstName?: string,
+	   lastName?: string,
+	   birthdate?: Date,
+	   mail?: string,
+	   gender?: string,
+	   mobile?: string,
+	   address?: string,
+	   balance?: number,
+	   userid?: number,
+	   token?: string
 	) {}
+	
   }
