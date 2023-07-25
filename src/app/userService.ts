dff5cf22da09f90ceb96dd41f3b3aa1187a08c1f
Youssef-zekry <youssef.zekry@mokalmat.com>
2023-07-25 16:39:54 +0300

    Add setUser method in service

--- src/app/userService.ts
+++ src/app/userService.ts
@@ -1,9 +1,12 @@
 import{user} from './models/user'
 
 export class userservice{
-	
+
 getUser():user{
 	const t = localStorage.getItem("user");
 	 return t? JSON.parse(t): null;
+}
+setUser(user: user){
+	localStorage.setItem('user', JSON.stringify(user));
 }
 }
