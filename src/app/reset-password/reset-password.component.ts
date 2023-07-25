05193caf8a7cd891831b63ce4f1aaf6035357b7f
Youssef-zekry <youssef.zekry@mokalmat.com>
2023-07-25 16:44:59 +0300

    Add reset password method.

--- src/app/reset-password/reset-password.component.ts
+++ src/app/reset-password/reset-password.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
-
+import{user} from '../models/user'
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-reset-password',
   templateUrl: './reset-password.component.html',
   styleUrls: ['./reset-password.component.css']
 })
 export class ResetPasswordComponent {
+  constructor(private http: HttpClient, private router: Router) { }
 
-}
+  resetPassword(mail:string, question:string, answer:string){
+    const url = 'http://localhost:8080/demo/resetPassword';
+    const body = {mail: mail, question_id:parseInt( question), answer: answer};
+    return this.http.post(url, body).pipe(map((response:any)=>{
+      return new user(
+        response.status_code,
+        response.message
+        );
+      })
+      ).subscribe(user => {
+        if (user.status_code === -1) {
+          console.log(body);
+          alert('Failed to reset password');
+        } else {
+          console.log(user);
+          alert('Password has been reset successfully, Please check your email for the new password')
+        }
+      });
+    }
+  }
+  
