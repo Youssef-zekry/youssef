abf2d373a0da2983394ad936823b9538c69cdc96
Youssef-zekry <youssef.zekry@mokalmat.com>
2023-07-26 12:29:09 +0300

    Remove declaration before constructor to avoid putting initial values that cause problem with login.

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,29 +1,17 @@
 export class user {
-	public status_code: number = -2;
-	public message: string = '';
-	public firstName: string = '';
-	public lastName: string = '';
-	public birthdate: Date | null = null;
-	public mail: string = '';
-	public gender: string = '';
-	public mobile: string = '';
-	public address: string = '';
-	public balance: number = 0;
-	public userid: number = 0;
-	public token: string = '';
 	constructor(
-	   status_code?: number,
-	   message?: string,
-	   firstName?: string,
-	   lastName?: string,
-	   birthdate?: Date,
-	   mail?: string,
-	   gender?: string,
-	   mobile?: string,
-	   address?: string,
-	   balance?: number,
-	   userid?: number,
-	   token?: string
+	  public status_code?: number,
+	  public message?: string,
+	  public firstName?: string,
+	  public lastName?: string,
+	  public birthdate?: Date,
+	  public mail?: string,
+	  public gender?: string,
+	  public mobile?: string,
+	  public address?: string,
+	  public balance?: number,
+	  public userid?: number,
+	  public token?: string
 	) {}
 	
   }
