ffc1c95a54f1ec9f61f5b64938fefe41f16617fe
Youssef-zekry <youssef.zekry@mokalmat.com>
2023-08-07 11:48:40 +0300

    Add product model

---
+++ src/app/models/product.ts
@@ -0,0 +1,9 @@
+export class product {
+  constructor(
+    public product_id?: number,
+    public product_name?: string,
+    public product_desc?: string,
+    public product_price?: number,
+    public stock?: number
+  ) {}
+}
