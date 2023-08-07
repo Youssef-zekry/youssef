7ceb2d689a8a2183346a56dee5492ffd59920cb7
Youssef-zekry <youssef.zekry@mokalmat.com>
2023-08-07 11:51:23 +0300

    Retrieve products from db and set it into products

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { product } from '../models/product';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { productService } from '../productService';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit{
+  products:product[]=[];
+  constructor(
+    private route: ActivatedRoute,
+    private productService: productService,
+    private http: HttpClient
+  ){}
 
+  ngOnInit(): void {
+    const url = 'http://localhost:8080/demo/products';
+    this.http.get<product[]>(url)
+    .subscribe(
+      
+      
+      response=>{
+        console.log(response);        
+        this.products = response;
+      }
+    );
+
+    // this.product = this.productService.getProduct();
+}
 }
