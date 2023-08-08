18c80e5dccd03fafba9ce1c157e4511c625b1f6c
Youssef-zekry <youssef.zekry@mokalmat.com>
2023-08-08 12:39:26 +0300

    Remove alerts

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -5,6 +5,9 @@ import { HttpClient } from '@angular/common/http';
 import { productService } from '../productService';
 import { userservice } from '../userService';
 import { user } from '../models/user';
+import { responseViewModel } from '../models/responseViewModel';
+
+
 
 @Component({
   selector: 'app-products',
@@ -18,6 +21,7 @@ export class ProductsComponent implements OnInit {
   user?: user;
   uid: any;
   uToken: any;
+  message: string ="";
 
   constructor(
     private route: ActivatedRoute,
@@ -44,7 +48,7 @@ export class ProductsComponent implements OnInit {
     } else {
       newItem = {
         type: product,
-        count: itemCount,
+        count: itemCount|| 1,
       };
       this.cart.push(newItem);
     }
@@ -66,19 +70,23 @@ export class ProductsComponent implements OnInit {
     };
     console.log(JSON.stringify(requestBody));
     const url = 'http://localhost:8080/demo/order';
-    this.http.post(url, requestBody).subscribe(
-      (response: any) => {
-        if (response.status_code === -1 || -2 || -3 || -4) {
-          alert('order failed');
-        }
-        else{
-          alert('ordered successfully')
-        }
-      },
 
-      (error: any) => {
-        console.error(error);
-      }
-    );
+    this.http.post<responseViewModel>(url, requestBody).subscribe(
+		(response) => {
+        if (response.status_code === -1||response.status_code === -2||response.status_code === -3||response.status_code === -4) {
+          console.log(response);
+		  this.message = "Order failed";
+        //   alert('order failed');
+
+        } else if (response.status_code === 0) {
+          console.log(response);
+		  this.message = "Order placed successfully";
+        //   alert('ordered successfully');
+        } else {
+          console.log(response);
+        //   alert('something went wrong');
+		this.message = "Something went wrong";
+        }
+      });
   }
 }
