db150039ab0ca3c5c68d6f7f6d63d09fa61474c9
Youssef-zekry <youssef.zekry@mokalmat.com>
2023-08-08 12:44:53 +0300

    Empty cart after attempting to order

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -7,8 +7,6 @@ import { userservice } from '../userService';
 import { user } from '../models/user';
 import { responseViewModel } from '../models/responseViewModel';
 
-
-
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
@@ -21,7 +19,7 @@ export class ProductsComponent implements OnInit {
   user?: user;
   uid: any;
   uToken: any;
-  message: string ="";
+  message: string = '';
 
   constructor(
     private route: ActivatedRoute,
@@ -48,7 +46,7 @@ export class ProductsComponent implements OnInit {
     } else {
       newItem = {
         type: product,
-        count: itemCount|| 1,
+        count: itemCount || 1,
       };
       this.cart.push(newItem);
     }
@@ -71,22 +69,28 @@ export class ProductsComponent implements OnInit {
     console.log(JSON.stringify(requestBody));
     const url = 'http://localhost:8080/demo/order';
 
-    this.http.post<responseViewModel>(url, requestBody).subscribe(
-		(response) => {
-        if (response.status_code === -1||response.status_code === -2||response.status_code === -3||response.status_code === -4) {
+    this.http
+      .post<responseViewModel>(url, requestBody)
+      .subscribe((response) => {
+        if (
+          response.status_code === -1 ||
+          response.status_code === -2 ||
+          response.status_code === -3 ||
+          response.status_code === -4
+        ) {
           console.log(response);
-		  this.message = "Order failed";
-        //   alert('order failed');
-
+          this.message = 'Order failed';
+          //   alert('order failed');
         } else if (response.status_code === 0) {
           console.log(response);
-		  this.message = "Order placed successfully";
-        //   alert('ordered successfully');
+          this.message = 'Order placed successfully';
+          //   alert('ordered successfully');
         } else {
           console.log(response);
-        //   alert('something went wrong');
-		this.message = "Something went wrong";
+          //   alert('something went wrong');
+          this.message = 'Something went wrong';
         }
+		this.cart = [];
       });
   }
 }
