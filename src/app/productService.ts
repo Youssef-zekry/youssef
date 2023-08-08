72a526735b6a1e057b9d3ec6153cc6abf90edc1b
Youssef-zekry <youssef.zekry@mokalmat.com>
2023-08-08 12:37:24 +0300

    Add set product (not needed atm)

--- src/app/productService.ts
+++ src/app/productService.ts
@@ -1,8 +1,13 @@
 import { product } from "./models/product";
 
 export class productService{
+	
 	getProduct(): product{
 		const t = localStorage.getItem('product');
 		return t ? JSON.parse(t) : null;
+	}
+
+	setProduct(product:product){
+		localStorage.setItem('product',JSON.stringify(product));
 	}
 }
