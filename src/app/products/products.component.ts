84f4d274739c646b6e6c8a892b25c8735ff883c8
Youssef-zekry <youssef.zekry@mokalmat.com>
2023-08-08 09:26:33 +0300

    1- Add cart page html 2- Add submit order function

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,50 +1,84 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { product } from '../models/product';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { productService } from '../productService';
+import { userservice } from '../userService';
+import { user } from '../models/user';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent implements OnInit{
-  products:product[]=[];
-  cart:any[]=[];
-  screenNumber:number = 1;
+export class ProductsComponent implements OnInit {
+  products: product[] = [];
+  cart: any[] = [];
+  screenNumber: number = 1;
+  user?: user;
+  uid: any;
+  uToken: any;
+
   constructor(
     private route: ActivatedRoute,
     private productService: productService,
-    private http: HttpClient
-  ){}
+    private http: HttpClient,
+    private userService: userservice
+  ) {}
 
   ngOnInit(): void {
     const url = 'http://localhost:8080/demo/products';
-    this.http.get<product[]>(url)
-    .subscribe(
-      response=>{
-        console.log(response);        
-        this.products = response;
-      }
-    );
-    // this.product = this.productService.getProduct();
+    this.http.get<product[]>(url).subscribe((response) => {
+      console.log(response);
+      this.products = response;
+    });
+    this.user = this.userService.getUser();
+    this.uid = this.userService.getUser().userid;
+    this.uToken = this.userService.getUser().token;
   }
 
-  addToCart(product:product,itemCount:number){
-    let newItem = this.cart.find((item:any) => item.type === product);
-    if(newItem) {
+  addToCart(product: product, itemCount: number) {
+    let newItem = this.cart.find((item: any) => item.type === product);
+    if (newItem) {
       newItem.count += itemCount;
     } else {
       newItem = {
         type: product,
-        count: itemCount
+        count: itemCount,
       };
-      this.cart.push(newItem)
+      this.cart.push(newItem);
     }
-
-    
     console.log(this.cart);
-    
+  }
+
+  submitOrder() {
+    const orderJsonList = this.cart.map((item: any) => {
+      return {
+        product_id: item.type.product_id,
+        user_id: this.uid,
+        item_count: item.count,
+      };
+    });
+
+    const requestBody = {
+      orderJsonList: orderJsonList,
+      token: this.uToken,
+    };
+    console.log(JSON.stringify(requestBody));
+    const url = 'http://localhost:8080/demo/order';
+    this.http.post(url, requestBody).subscribe(
+      (response: any) => {
+        if (response.status_code === -1 || -2 || -3 || -4) {
+          alert('order failed');
+        }
+        else{
+          alert('ordered successfully')
+        }
+      },
+
+      (error: any) => {
+        console.error(error);
+      }
+    );
   }
 }
